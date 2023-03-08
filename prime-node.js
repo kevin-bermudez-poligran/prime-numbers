0f44018fe9ed3a04513b5439649318036294f211
kevin-bermudez-poligran <kebermudez3@poligran.edu.co>
2023-03-07 22:39:06 -0500

    refactor: algorithm in function is prime

--- prime-node.js
+++ prime-node.js
@@ -1,11 +1,18 @@
-const number = process.argv[2]
-let isPrime = true
+/*En matemáticas, un número primo es un número natural mayor que 1 que tiene únicamente dos divisores positivos distintos: él mismo y el 1.*/
 
-for (let i = 2; i < number; i++){
-  if (number % i === 0) {
-    isPrime = false;
-    break;
-  }
+const numberConsole = process.argv[2]
+
+function numberIsPrime(number) {
+  let isPrime = true
+
+  for (let i = 2; i < number; i++){
+    if (number % i === 0) {
+      isPrime = false;
+      break;
+    }
+  } 
+
+  return isPrime
 }
 
-console.log('number',number,isPrime ? 'is' : 'is not','prime')
+console.log('number',numberConsole,numberIsPrime(numberConsole) ? 'is' : 'is not','prime')
